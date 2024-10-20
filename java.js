cddc5dc35cea3015a4aae743587dc1b9e3adf431
Nguyễn Hà Quỳnh Giao <21110171@student.hcmute.edu.vn>
2024-10-20 18:53:26 +0700

    Completed

---
+++ java.js
@@ -0,0 +1,22 @@
+onload = () => {
+    const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+    }, 1000);
+};
+
+var typed = new Typed('.auto-type', {
+    strings: [
+        "You are beautiful ❤️", 
+        "You are brave ⭐", 
+        "You are a ray of sunshine in my life. ☀️",
+        "You deserve everything good in life. 🎁",
+        "You'll do what you want 🎉",
+        "I wish you all the happiness in the world. 😊", 
+        "Happy Women’s Day 20/11! 🌸"
+    ],
+    typeSpeed: 50,  
+    backSpeed: 50,
+    loop: true
+});
+
